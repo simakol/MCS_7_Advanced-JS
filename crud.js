ac0457a7548b5bb1c7808a873764a0178312e805
Alexey Simak <>
2025-03-13 19:25:54 +0200

    feat: :sparkles: lesson-06

---
+++ crud.js
@@ -0,0 +1,61 @@
+/*
+ * Використовуємо сервіс https://mockapi.io/ для створення бекенду
+ */
+
+/*
+ * Read (GET) 
+ */
+
+function getBooks() {}
+
+function getBookById(id) {}
+
+// getBooks().then(console.log).catch(console.error);
+// getBookById(5).then(console.log).catch(console.error);
+// getBookById(55).then(console.log).catch(console.error);
+
+/*
+ * Create (POST)
+ */
+
+function addNewBook(book) {}
+
+// addNewBook({
+//   title: 'Тестова книга з CSS',
+//   author: 'Я',
+//   genres: ['CSS'],
+//   rating: 9,
+// })
+//   .then(console.log)
+//   .catch(console.error);
+
+// getBooks().then(console.log).catch(console.error);
+
+/*
+ * Update (PUT/PATCH)
+ */
+
+function updateBookById(id, fieldsToUpdate) {}
+
+// updateBookById(12, {
+//   genres: ['Fiction', 'Psyhology'],
+//   rating: 10,
+// })
+//   .then(console.log)
+//   .catch(console.error);
+
+// updateBookById(100, {
+//   genres: ['Fiction', 'Psyhology'],
+//   rating: 10,
+// })
+//   .then(console.log)
+//   .catch(console.error);
+
+/*
+ * Delete (DELETE)
+ */
+
+function deleteBookById(id) {}
+
+// deleteBookById(13).then(console.log).catch(console.error);
+// deleteBookById(13).then(console.log).catch(console.error);
